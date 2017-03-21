a518b3a34caa10e92c6bffc6edb3ac80cf63840b
harryxa <harry.alcott@hotmail.co.uk>
2017-03-21 19:05:17 +0000

    First commit

---
+++ TronGameW32/Code/Server/TronServerW32.cpp
@@ -0,0 +1,17 @@
+// TronServerW32.cpp : Defines the entry point for the console application.
+//
+
+#include <Server/stdafx.h>
+#include <iostream>
+
+int main()
+{
+	std::cout << "Searching for life signs...";
+	while (true)
+	{
+		std::cin.get();
+	}
+
+    return 0;
+}
+
